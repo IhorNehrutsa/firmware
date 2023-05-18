a608db8397b60cc54a37d23429c0bf6d9ecbc6dc
Ihor Nehrutsa <Ihor.Nehrutsa@gmail.com>
2023-05-18 08:29:31 +0300

    Add speex configures
    Copy config.h file to the speex repository: speex\include\speex\config.h
    Copy library.json file to the speex repository: speex\library.json

---
+++ speex/config.h
@@ -0,0 +1,12 @@
+/*
+ * Copy config.h file to the speex repository: speex\include\speex\config.h
+ * Copy library.json file to the speex repository: speex\library.json
+ */
+#ifndef CONFIG_H
+#define CONFIG_H
+
+#define EXPORT
+#define FIXED_POINT 1
+#define USE_SMALLFT
+
+#endif /* ! CONFIG_H */
