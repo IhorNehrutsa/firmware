ccf0282b049c7b2cfff4470f684095eb8fa74f87
Ihor Nehrutsa <Ihor.Nehrutsa@gmail.com>
2023-05-24 15:39:12 +0300

    Update w_ptd.bat

--- w_ptd.bat
+++ w_ptd.bat
@@ -1,3 +1,3 @@
-python.exe d:\MicroPython\esptool\esptool.py --chip esp32 --baud 921600 --flash_mode dio --flash_freq 80m write_flash 0x10000 D:\LoRa\MESHTASTIC\firmware\.pio\build\ptd/firmware.bin
+python.exe d:\MicroPython\esptool\esptool.py --chip esp32 --baud 921600 write_flash 0x10000 D:\LoRa\MESHTASTIC\firmware\.pio\build\ptd/firmware.bin
 
 pause
